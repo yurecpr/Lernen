addf2c7bebc8477c286a23f750610dc61c6ac9dc
Yurii Bolharyn <yurecpr@gmail.com>
2023-11-27 17:01:27 +0100

    Fibonacci

--- src/Lernen/Massiv/Fibonachhi.java
+++ src/Lernen/Massiv/Fibonachhi.java
@@ -3,16 +3,19 @@ package Lernen.Massiv;
 public class Fibonachhi {
     public static void main(String[] args) {
 
-        System.out.println(fibonacci(0));
-        System.out.println(fibonacci(1));
-        System.out.println(fibonacci(2));
-        System.out.println(fibonacci(3));
-        System.out.println(fibonacci(4));
+//        System.out.println(fibonacci(0));
+//        System.out.println(fibonacci(1));
+//        System.out.println(fibonacci(2));
+//        System.out.println(fibonacci(3));
+//        System.out.println(fibonacci(4));
         System.out.println(fibonacci(5));
-        System.out.println(fibonacci(6));
-        System.out.println(fibonacci(7));
-        System.out.println(fibonacci(7));
-        System.out.println(fibonacci(7));
+//        System.out.println(fibonacci(6));
+//        System.out.println(fibonacci(7));
+//        System.out.println(fibonacci(8));
+//        System.out.println(fibonacci(9));
+//        System.out.println(fibonacci(15));
+//        System.out.println(fibonacci(30));
+
 
     }
 
@@ -28,6 +31,7 @@ public class Fibonachhi {
             fNew = fib1 + fib2;
             fib1 = fib2;
             fib2 = fNew;
+           
 
         }
         return fNew;
