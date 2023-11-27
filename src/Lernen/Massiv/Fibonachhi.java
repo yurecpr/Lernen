1308e76f72218fb61e8ae3ce4855e42dad3144d7
Yurii Bolharyn <yurecpr@gmail.com>
2023-11-27 16:04:17 +0100

    Fibonacci

---
+++ src/Lernen/Massiv/Fibonachhi.java
@@ -0,0 +1,35 @@
+package Lernen.Massiv;
+
+public class Fibonachhi {
+    public static void main(String[] args) {
+
+        System.out.println(fibonacci(0));
+        System.out.println(fibonacci(1));
+        System.out.println(fibonacci(2));
+        System.out.println(fibonacci(3));
+        System.out.println(fibonacci(4));
+        System.out.println(fibonacci(5));
+        System.out.println(fibonacci(6));
+        System.out.println(fibonacci(7));
+        System.out.println(fibonacci(7));
+        System.out.println(fibonacci(7));
+
+    }
+
+    public static int fibonacci(int n) {
+        if (n < 2)
+            return n;
+
+        int fib1 = 0;
+        int fib2 = 1;
+        int fNew = 0;
+
+        for (int i = 2; i <= n; i++) {
+            fNew = fib1 + fib2;
+            fib1 = fib2;
+            fib2 = fNew;
+
+        }
+        return fNew;
+    }
+}
