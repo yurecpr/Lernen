42426750b81bfb94c24ca5c9e63b0dea8e28135a
Yurii Bolharyn <yurecpr@gmail.com>
2023-11-27 15:03:06 +0100

    ArrayFruits

--- src/HA/Lesson13/task1.java
+++ src/HA/Lesson13/task1.java
@@ -1,4 +1,17 @@
 package HA.Lesson13;
 
 public class task1 {
+    public static void main(String[] args) {
+        System.out.println("Arrey nums in loop");
+        String[] fruits = new String[]{"Orange", "Apple", "Banana", "Mango"};
+        for (int i = 0; i < fruits.length; i++) {
+            System.out.println("nums[" + i + "] = " + fruits[i]);
+        }
+        System.out.print(fruits[0] + " => ");
+        fruits[0] = "Kiwi";
+        System.out.println(fruits[0]);
+        for (int i = 0; i < fruits.length; i++) {
+            System.out.println("nums[" + i + "] = " + fruits[i]);
+        }
+    }
 }
