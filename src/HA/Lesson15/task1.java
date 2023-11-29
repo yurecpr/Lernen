1ef03bf80cbcb49409348f7bcaa957a9445c2642
Yurii Bolharyn <yurecpr@gmail.com>
2023-11-29 17:27:55 +0100

    FindTen

--- src/HA/Lesson15/task1.java
+++ src/HA/Lesson15/task1.java
@@ -2,18 +2,23 @@ package HA.Lesson15;
 
 public class task1 {
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 0, 0, 8, 10, 0, 22};
-        System.out.println(findTen(array));
+        int[] array = {1, 2, 3, 4, 0, 6, 10, 10, 2};
+        System.out.println(calcEvenNumbers(array));
+
     }
 
-    public static boolean findTen(int[] array) {
+    public static boolean calcEvenNumbers(int[] array) {
         boolean res = false;
-
+        int counter = 0;
         for (int i = 0; i < array.length; i++) {
             if (array[i] == 10) {
+                counter++;
+            }
+            if (counter == 3) {
                 res = true;
             }
         }
         return res;
     }
+
 }
