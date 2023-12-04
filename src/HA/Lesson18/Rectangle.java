430ea811ba6a7b85a4312047fed3f3c66582b6f0
Yurii Bolharyn <yurecpr@gmail.com>
2023-12-04 20:07:48 +0100

    OOP

---
+++ src/HA/Lesson18/Rectangle.java
@@ -0,0 +1,12 @@
+package HA.Lesson18;
+
+public class Rectangle {
+    int longed;
+    int width;
+
+    public Rectangle(int longed, int width) {
+        System.out.println("Long " + longed + " Width " + width + " Area of square= " + (longed * width));
+        System.out.println("Long " + longed + " Width " + width + " Perimeter of square= " + ((longed + width) * 2));
+    }
+
+}
---
+++ src/HA/Lesson18/main.java
@@ -0,0 +1,11 @@
+package HA.Lesson18;
+
+public class main {
+    public static void main(String[] args) {
+        Rectangle r1 = new Rectangle(12, 3);
+
+
+    }
+
+
+}
