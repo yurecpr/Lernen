877eed75b9087bccdef54d03c5d27968c019812f
Yurii Bolharyn <yurecpr@gmail.com>
2023-12-05 16:38:00 +0100

    OOP_FtoC

--- src/HA/Lesson19/Temperature.java
+++ src/HA/Lesson19/Temperature.java
@@ -1,6 +1,14 @@
 package HA.Lesson19;
 
 public class Temperature {
+    public static void main(String[] args) {
+
+        Temperature a = new Temperature(150);
+        Temperature b = new Temperature(100);
+        System.out.println(a.far + "°F= " + a.transform() + "°C");
+        System.out.println(b.far + "°F= " + b.transform() + "°C");
+
+    }
 
     int cells;
     int far;
