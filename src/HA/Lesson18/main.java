ec265b5da468ac309935daa0bf731e042dc6bfe3
Yurii Bolharyn <yurecpr@gmail.com>
2023-12-05 10:00:53 +0100

    OOP

--- src/HA/Lesson18/main.java
+++ src/HA/Lesson18/main.java
@@ -3,6 +3,7 @@ package HA.Lesson18;
 public class main {
     public static void main(String[] args) {
         Rectangle r1 = new Rectangle(12, 3);
+        Rectangle r2 = new Rectangle(24, 5);
 
 
     }
