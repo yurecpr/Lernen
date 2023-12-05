5e2ec577a47de08945b9ec85c7b477849c9731fb
Yurii Bolharyn <yurecpr@gmail.com>
2023-12-05 09:58:43 +0100

    OOP

--- src/HA/Lesson18/Student.java
+++ src/HA/Lesson18/Student.java
@@ -3,18 +3,25 @@ package HA.Lesson18;
 public class Student {
     String name;
     int age;
+    static int count = 0;
 
     public static void main(String[] args) {
         Student st1 = new Student();
         st1.name = "Ivan";
         st1.age = 21;
+        count++;
+
         System.out.println("Student name: " + st1.name + ", Age: " + st1.age);
+        System.out.println("Total student: " + count);
 
         Student st2 = new Student();
         st2.name = "Anna";
         st2.age = 20;
-        System.out.println("Student name: " + st2.name + ", Age: " + st2.age);
+        count++;
 
+
+        System.out.println("Student name: " + st2.name + ", Age: " + st2.age);
+        System.out.println("Total student: " + count);
     }
 }
 
