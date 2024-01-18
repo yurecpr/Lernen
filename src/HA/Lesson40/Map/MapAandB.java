1bdb0538ea088bd5c3a37829857d7da0b7aa9df8
Yurii Bolharyn <yurecpr@gmail.com>
2024-01-18 09:17:16 +0100

    Map

--- src/HA/Lesson40/Map/MapAandB.java
+++ src/HA/Lesson40/Map/MapAandB.java
@@ -6,20 +6,19 @@ import java.util.Map;
 public class MapAandB {
 
     public static Map<String, String> mapAB(Map<String, String> inputMap) {
-        // Создаем новую Map для хранения результата
+
         Map<String, String> resultMap = new HashMap<>(inputMap);
 
-        // Проверяем наличие ключей 'a' и 'b' в исходной Map
+
         if (inputMap.containsKey("a") && inputMap.containsKey("b")) {
-            // Получаем значения по ключам 'a' и 'b'
+
             String valueA = inputMap.get("a");
             String valueB = inputMap.get("b");
 
-            // Создаем новый ключ 'ab' с суммой значений от ключей 'a' и 'b'
             resultMap.put("ab", valueA + valueB);
         }
 
-        // Возвращаем результат
+
         return resultMap;
     }
 
